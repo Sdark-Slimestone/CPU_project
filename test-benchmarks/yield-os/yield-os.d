31fa44d2c0c2ed024b47d16396399195822b88b9
0-Tan ZiJian <3047808825@qq.com>
2026-07-04 13:42:41 +0800

    track all files except yosys-sta toolchain (too large)

---
+++ test-benchmarks/yield-os/yield-os.d
@@ -0,0 +1,6 @@
+/home/sdark/ysyx-workbench/am-kernels/kernels/yield-os/build/riscv32e-npc/yield-os.o: \
+ /home/sdark/ysyx-workbench/am-kernels/kernels/yield-os/yield-os.c \
+ /home/sdark/ysyx-workbench/abstract-machine/am/include/am.h \
+ /home/sdark/ysyx-workbench/abstract-machine/am/include/arch/riscv.h \
+ /home/sdark/ysyx-workbench/abstract-machine/am/include/amdev.h \
+ /home/sdark/ysyx-workbench/abstract-machine/klib/include/klib-macros.h
